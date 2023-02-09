45c49f9f0df8787b9af9761f1b52a965cffd3d18
Gopi2312 <123471487+Gopi2312@users.noreply.github.com>
2023-02-09 09:42:38 +0530

    commit

--- ds.js
+++ ds.js
@@ -7,7 +7,7 @@ console.log("1.Concat",arr1.concat(arr2));
 console.log("2.CopyWithin",arr1.copyWithin(1,0,2));
 
 //entries
-trr1 = arr1.entries();
+let trr1 = arr1.entries();
 arr1.forEach(value => {
     console.log("3.Entries",trr1.next().value);
 });
@@ -21,15 +21,15 @@ function check(x)
 
 //fill
 console.log(arr1);
-trr2 = arr1.fill(0,2);
+let trr2 = arr1.fill(0,2);
 console.log("5.fill",trr2);
 
 //filter
-arr3 = [5,7,8,9];
+let arr3 = [5,7,8,9];
 console.log("6.filter",arr3.filter(x=> x<=5));
 
 //find
-arr4 = ["gopi","vasu","hari","sabari"];
+let arr4 = ["gopi","vasu","hari","sabari"];
 console.log("7.find",arr4.find(x => x.length <5));
 
 //findindex
@@ -76,34 +76,34 @@ console.log("16.lastIndexOf",arr5.lastIndexOf(6));
 console.log("17.length",arr5.length);
 
 //map
-num = arr5.map(x=> x+4);
+let num = arr5.map(x=> x+4);
 console.log("18.map",num);
 
 //pop
 console.log("19.pop",arr5.pop());
 
 //push
-s = arr5.push(5);
+let s = arr5.push(5);
 console.log("20.push",arr5);
 
 //reverse
-g=["gopii","df","vb","fdfg"];
+let g=["gopii","df","vb","fdfg"];
 console.log("21.reverse",g.reverse());
 
 //shift
-d=g.shift();
+let d=g.shift();
 console.log("22.shift",g);
 
 //unshift
-f = g.unshift("dupi");
+let f = g.unshift("dupi");
 console.log("23.unshift",g);
 
 //splice
-trr6 = g.splice(0,1,"mohan");
+let trr6 = g.splice(0,1,"mohan");
 console.log("24.splice",g);
 
 //sort
-arr7 = [2,7,3,90,12,5];
+let arr7 = [2,7,3,90,12,5];
 arr7.sort(function(a,b)
 {
     return a - b;
@@ -111,7 +111,7 @@ arr7.sort(function(a,b)
 console.log("25.sort",arr7);
 
 //reduce
-arr8 = [20,10,5];
+let arr8 = [20,10,5];
 console.log("26.reduce",arr8.reduce(gopi));
 console.log("27reduceRight",arr8.reduceRight(gopi));
 function gopi(a,b)
